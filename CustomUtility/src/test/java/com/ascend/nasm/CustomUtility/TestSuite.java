91d928e94b7ef11b5ab072b284157cfd3aca5c91
Manish Sharma <manish.sharma2@happiestminds.com>
2019-04-08 13:15:33 +0530

    Added testsuite

---
+++ CustomUtility/src/test/java/com/ascend/nasm/CustomUtility/TestSuite.java
@@ -0,0 +1,10 @@
+package com.ascend.nasm.CustomUtility;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses ({DBConnectTest.class,
+		StringUtilTest.class})
+public class TestSuite {}
