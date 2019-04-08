1437851916469c41ec104c5a3eca37d9580608bb
Manish Sharma <manish.sharma2@happiestminds.com>
2019-04-08 13:28:06 +0530

    test

--- CustomUtility/src/test/java/com/ascend/nasm/CustomUtility/TestSuite.java
+++ CustomUtility/src/test/java/com/ascend/nasm/CustomUtility/TestSuite.java
@@ -4,6 +4,11 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
+/**
+ * 
+ * @author Manish.sharma2
+ *
+ */
 @RunWith(Suite.class)
 @SuiteClasses ({DBConnectTest.class,
 		StringUtilTest.class})
